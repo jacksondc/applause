5103fd30ac717416a5d2964635b31cd5ff6ecf27
Jackson de Campos <jacksondecampos@gmail.com>
2014-07-26 19:47:55 -0400

    Update styles, example URL

--- router.js
+++ router.js
@@ -1,8 +1,12 @@
+Router.configure({
+  layoutTemplate: 'layout'
+})
+
 Router.map(function () {
   this.route('main', {
   	path: '/'
   });
   this.route('search', {
     path: '/search'
-  })
+  });
 });
