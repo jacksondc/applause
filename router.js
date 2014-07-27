71c04610d6aefd502d83863a6cf90ee91a21639a
Jackson de Campos <jacksondecampos@gmail.com>
2014-07-26 20:22:31 -0400

    Removed meteor files

--- router.js
+++
@@ -1,8 +0,0 @@
-Router.map(function () {
-  this.route('main', {
-  	path: '/'
-  });
-  this.route('search', {
-    path: '/search'
-  })
-});
--- server/search.js
+++
@@ -1,6 +0,0 @@
-Meteor.methods({
-  'search': function(query) {
-    var page = Pages.findOne({url: query});
-    return page ? page.votes : 0;
-  }
-});
